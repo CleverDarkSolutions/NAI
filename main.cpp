b599ed47da3f9016b23b1285ecc20387ea1f2556
CleverDarkSolutions <66365110+CleverDarkSolutions@users.noreply.github.com>
2022-10-11 18:18:23 +0200

    New version

--- main.cpp
+++ main.cpp
@@ -1,34 +1,42 @@
-#include <iostream>
 #include <functional>
-#include <any>
+#include <iostream>
 #include <map>
-#include <vector>
 #include <string>
-#include <complex>
-#include <algorithm>
-
-int main(int argc, char **argv) {
-    std::cout<<"Test"<<std::endl;
-
+#include <vector>
+#include <cmath>
+using myfunction_t = std::function<double(std::vector<double>)>;
+void calculate(std::vector<double> numbers, myfunction_t func) {
     using namespace std;
-    vector<double> arguments;
-
-//    transform(argv+2, argv+argc, back_inserter(arguments), [](auto s){return stod(s);});
-
-    for (int i = 2; i < argc; i++){
-        arguments.push_back(stod(argv[i]));
+    cout << func(numbers) << endl;
+}
+int main(int argc, char **argv) {
+    using namespace std;
+    map<string, myfunction_t> formaters;
+    formaters["mod"] = [](vector<double> numbers) { return (int)numbers.front() % (int)numbers.back();};
+    formaters["add"] = [](vector<double> numbers) { return  numbers.front() + numbers.back();};
+    formaters["sin"] = [](vector<double> numbers) { return sin(numbers.front());};
+    try {
+        vector<string> arguments(argv, argv + argc);
+        auto chosen_function = arguments.at(2);
+        if (arguments.size() > 5){
+            cout << "Too much args " << arguments.size() - 1;
+            return 1;
+        }
+        else if(chosen_function == "sin" && arguments.size() > 4){
+            cout << "Too much args, sin takes only one";
+            return 1;
+        }
+        else if (arguments.at(1) != "lab1"){
+            cout << "No lab1 specified at the beginning";
+            return 1;
+        }
+        vector<double> numbers = {{stod(arguments.at(3)), stod(arguments.back())}};
+        (numbers, formaters.at(chosen_function));
+    } catch (std::out_of_range aor) {
+        cout << "Incorrect arg. Choose from: ";
+        for (auto [k, v] : formaters) cout << " " << k;
+        cout << endl;
+        return 1;
     }
-
-    for (auto e : arguments){
-        cout << "func: " << argv[1] << endl;
-        cout << "arg: " << e << endl;
-    }
-    map<string, function< double(vector<double>)> > myFunctions;
-    myFunctions["add"] = [](vector<double> arg) { return arg.at(0) + arg.at(1); };
-    myFunctions["sin"] = [](vector<double> arg) { return sin(arg.at(0)); };
-    myFunctions["mod"] = [](vector<double> arg) {return (int)arg.at(0) % (int)arg.at(1);};
-
-
-
     return 0;
 }
