5beeba7c5a741c029f90d6c2002bc5e54bd1b847
CleverDarkSolutions <RotmgADAS@o2.pl>
2022-11-30 13:46:58 +0100

    zad6 done

--- main.cpp
+++ main.cpp
@@ -11,6 +11,23 @@ std::random_device rd;
 std::mt19937 mt_generator(rd());
 using chromosome_t = std::vector<int>;
 using population_t = std::vector<chromosome_t>;
+using myTerm = std::function<bool(population_t a, vector<double> b, int iterCount, int iteration)>;
+
+double findStandardDeviation(vector<double> numbers) {
+    float sum = 0.0, sDeviation = 0.0, mean;
+    int i;
+
+    for(i = 0; i < numbers.size(); i++) {
+        sum += numbers.at(i);
+    }
+    mean = sum/numbers.size();
+
+    for(i = 0; i < numbers.size(); ++i) {
+        sDeviation += pow(numbers.at(i) - mean, 2);
+    }
+
+    return sqrt(sDeviation/numbers.size());
+}
 
 double scale(double number, double oldMax, double oldMin, double newMin, double newMax)
 {
@@ -30,6 +47,30 @@ population_t populate(int popSize, int chromSize){
         population.push_back(chromosome);
     }
     return population;
+}
+
+vector<double> popStatistics(vector<double> fitness){
+    double max = 0;
+    double min = fitness.at(0);
+    double fitSum = 0;
+    for (auto elem : fitness){
+        fitSum += elem;
+        if(elem > max){
+            max = elem;
+        }else if (elem < min){
+            min = elem;
+        }
+    }
+
+    double fitAvg = fitSum / fitness.size();
+
+    vector<double> resVector;
+
+    resVector.push_back(min);
+    resVector.push_back(max);
+    resVector.push_back(fitAvg);
+
+    return resVector;
 }
 
 pair<double, double> translate(chromosome_t chromosome){
@@ -58,26 +99,29 @@ pair<double, double> translate(chromosome_t chromosome){
     if(flagNegativeY){
         result.second*=-1;
     }
-    result.first = result.first/1000000000000000;
-    result.second = result.second/1000000000000000;
+    result.first = result.first/10000000000000000;
+    result.second = result.second/10000000000000000;
     return result;
 }
 
 auto genetic_algorithm = [](
         auto initial_population, auto fitness, auto term_condition,
         auto selection,
+        auto iterCount,
         double p_crossover,
         auto crossover,
         double p_mutation,
         auto mutation,
         myfunction_t function,
         vector<double> domain,
-        double goal){
+        double goal,
+        bool print){
     using namespace std;
+    int iteration = 0;
     uniform_real_distribution<double> uniform(0.0,1.0);
     auto population = initial_population;
-    vector<double> population_fit = fitness(population, function, domain, goal);
-    while (!term_condition(population,population_fit)) {
+    vector<double> population_fit = fitness(population, function, domain, goal, print, iteration);
+    while (!term_condition(population,population_fit, iterCount, iteration)) {
         auto parents_indexes = selection(population_fit);
         decltype(population) new_population;
         for (int i = 0 ; i < parents_indexes.size(); i+=2) {
@@ -91,11 +135,14 @@ auto genetic_algorithm = [](
             chromosome = mutation(chromosome,p_mutation);
         }
         population = new_population;
-        population_fit = fitness(population, function, domain, goal);
+        population_fit = fitness(population, function, domain, goal, print, iteration);
+
+
+        iteration++;
     }
     return population;
 };
-std::vector<double> fitness_function(population_t pop, myfunction_t function, vector<double> domain, double goal){
+std::vector<double> fitness_function(population_t pop, myfunction_t function, vector<double> domain, double goal, bool print, int iteration){
     vector<double> result;
     pair <double, double> currPair;
     for (int i = 0; i < pop.size(); i++){
@@ -107,13 +154,13 @@ std::vector<double> fitness_function(population_t pop, myfunction_t function, ve
             result.push_back(1 - scale(abs(currPair.first) ,100, domain.at(1), 0, 1) + 1 - scale(abs(currPair.second) ,100, domain.at(1), 0, 1));
         }
     }
-    int goodCount = 0;
-    for (double p: result) {
-        if(p>9990){
-            goodCount++;
-        }
+    if (print){
+        vector<double> toPrint = popStatistics(result);
+        cout << "iteration: " << iteration << endl;
+        cout << "min: " << toPrint.at(0) << endl;
+        cout << "max: " << toPrint.at(1) << endl;
+        cout << "avg: " << toPrint.at(2) << endl;
     }
-    cout << goodCount;
     return result;
 }
 std::vector<int> selection(std::vector<double> fitnesses) {
@@ -149,15 +196,13 @@ std::vector<chromosome_t > crossover_empty(std::vector<chromosome_t > parents) {
 }
 chromosome_t mutation_empty(chromosome_t parent, double p_mutation) {
     uniform_real_distribution<> randomNumb(0.0,1.0);
-    uniform_real_distribution<> randomCount(0, 5);
-    uniform_real_distribution<> randomPoint(0, parent.size());
-    if(randomNumb(mt_generator) < p_mutation){
-        for (int i = 0; i < randomCount(mt_generator); i++) {
-            int currPoint = randomPoint(mt_generator);
-            if (parent.at(currPoint) == 0){
-                parent.at(currPoint) = 1;
+
+    for (int i = 0; i < parent.size(); i++) {
+        if(randomNumb(mt_generator) < p_mutation){
+            if (parent.at(i) == 0){
+                parent.at(i) = 1;
             } else{
-                parent.at(currPoint) = 0;
+                parent.at(i) = 0;
             }
         }
     }
@@ -165,9 +210,17 @@ chromosome_t mutation_empty(chromosome_t parent, double p_mutation) {
     return parent;
 }
 
-int main(int argc, char **argv){
+int main(int argc, char *argv[]){
+    int popSize = 1000;
+    int iterCount = 1000;
+    double crossChance = 0;
+    double mutateChance = 0;
+    auto selectedFunction = "beale";
+    auto selectedTerm = "standard";
+    bool toPrint = false;
 
     map<string, myfunction_t> myFunctions;
+    map<string, myTerm> termConditions;
     map<string, vector<double>> domain;
     map<string, double> goal;
 
@@ -183,6 +236,18 @@ int main(int argc, char **argv){
     myFunctions["matyas"] = [](pair<double, double> xy) {
         return (0.26 * (pow(xy.first, 2) + pow(xy.second, 2)) - (0.48 * xy.first * xy.second));
     };
+    termConditions["standard"] = [](auto a, auto b, int iterCount, int iteration) {
+        return iteration > iterCount;
+    };
+    termConditions["custom"] = [](auto a, auto b, int iterCount, int iteration) {
+        if (findStandardDeviation(b) <= 0.0001){
+            cout << "deviaton: "<< findStandardDeviation(b) << endl;
+            return true;
+        } else{
+            return false;
+        }
+    };
+
 
     domain["beale"] = {-4.5,4.5};
     domain["himmel"] = {-5,5};
@@ -194,30 +259,44 @@ int main(int argc, char **argv){
     goal["threeHumpCamel"] = 0;
     goal["matyas"] = 0;
 
-   population_t population = populate(1000, 100+(23315%10)*2);
+    population_t population = populate(popSize, 100+(23315%10)*2);
+    try {
+        for (int i = 1; i < argc; i += 2) {
+            if (i + 1 != argc) {
+                if (string(argv[i]) == "-p") {
+                    popSize = atoi(argv[i + 1]);
+                } else if (string(argv[i]) == "-i") {
+                    iterCount = atoi(argv[i + 1]);
+                } else if (string(argv[i]) == "-c") {
+                    crossChance = atof(argv[i + 1]);
+                } else if (string(argv[i]) == "-m") {
+                    mutateChance = atof(argv[i + 1]);
+                } else if (string(argv[i]) == "-f") {
+                    selectedFunction = argv[i + 1];
+                } else if (string(argv[i]) == "-t") {
+                    selectedTerm = argv[i + 1];
+                } else if (string(argv[i]) == "-s") {
+                    toPrint = atoi(argv[i + 1]);
+                } else {
+                    cout << "invalid argument: " << argv[i] << endl;
+                    return 1;
+                }
+            }
+        }
 
-
-   try {
-       vector<string> arguments(argv, argv + argc);
-       auto selectedFunction = arguments.at(1);
-       population = genetic_algorithm(population,
+        population = genetic_algorithm(population,
                                        fitness_function,
-                                       [](auto a, auto b) {
-                                           for (auto elem: b) {
-                                                if(elem > 9999){
-                                                    return true;
-                                                }
-                                           };
-                                           return false;
-                                           },
+                                       termConditions.at(selectedTerm),
                                        selection,
-                                       1.0,
+                                       iterCount,
+                                       crossChance,
                                        crossover_empty,
-                                       0.05,
+                                       mutateChance,
                                        mutation_empty,
                                        myFunctions.at(selectedFunction),
                                        domain.at(selectedFunction),
-                                       goal.at(selectedFunction));
+                                       goal.at(selectedFunction),
+                                       toPrint);
    }
    catch (std::out_of_range aor) {
        cout << "Wrong arg. Choose from: ";
